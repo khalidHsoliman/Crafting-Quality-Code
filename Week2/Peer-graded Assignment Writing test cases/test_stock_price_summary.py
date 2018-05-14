a19aa03f437c620273a606472114323d989d9722
Khalid Hassan <khalidhsoliman@gmail.com>
2018-05-14 04:01:33 +0200

    Add test cases for strock_price_summary()

--- Week2/Peer-graded Assignment Writing test cases/test_stock_price_summary.py
+++ Week2/Peer-graded Assignment Writing test cases/test_stock_price_summary.py
@@ -6,7 +6,41 @@ class TestStockPriceSummary(unittest.TestCase):
     """ Test class for function a1.stock_price_summary. """
 
     # Add your test methods for a1.stock_price_summary here.
+    def test_stock_price_empty(self):
+        """ test stock_price_summary with an empty list """
+
+        actual = a1.stock_price_summary([])
+        expected = (0, 0)
+        self.assertEqual(actual,expected) 
+
+    def test_stock_price_zero(self):
+        """ test stock_price_summary with a list of one element 0"""
+
+        actual = a1.stock_price_summary([0])
+        expected = (0, 0)
+        self.assertEqual(actual,expected)
+
+    def test_stock_price_postive(self):
+        """ test stock_price_summary with a list of only postive"""
+
+        actual = a1.stock_price_summary([0.01,0.03,0,0.10,0])
+        expected = (0.14, 0)
+        self.assertEqual(actual,expected)
 
 
+    def test_stock_price_negative(self):
+        """ test stock_price_summary with a list of only negative"""
+
+        actual = a1.stock_price_summary([-0.02,-0.14,-0.01])
+        expected = (0, -0.17)
+        self.assertEqual(actual,expected)
+
+    def test_stock_price_both(self):
+        """ test stock_price_summary with a list of both postive and negative"""
+
+        actual = a1.stock_price_summary([0.01, 0.03, -0.02, -0.14, 0, 0, 0.10, -0.01])
+        expected = (0.14, -0.17)
+        self.assertEqual(actual,expected)
+        
 if __name__ == '__main__':
     unittest.main(exit=False)
