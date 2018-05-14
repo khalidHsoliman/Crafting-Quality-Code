77d4116d5e5ff5227f0dfc96cac9394de33941c6
Khalid Hassan <khalidhsoliman@gmail.com>
2018-05-15 01:38:11 +0200

    Add an implementation for __eq__ special method

--- Week4/CashRegister.py
+++ Week4/CashRegister.py
@@ -5,18 +5,6 @@ class CashRegister:
         """ (CashRegister, int, int, int, int, int) -> NoneType
 
         A CashRegister with loonies, toonies, fives, tens, and twenties.
-        
-        >>> register = CashRegister(5, 5, 5, 5, 5)
-        >>> register.loonies
-        5
-        >>> register.toonies
-        5
-        >>> register.fives
-        5
-        >>> register.tens
-        5
-        >>> register.twenties
-        5
         """
 
         self.loonies = loonies
@@ -25,14 +13,23 @@ class CashRegister:
         self.tens = tens
         self.twenties = twenties
 
+    def __eq__(self, other):
+        """ (CashRegister, CashRegister) -> bool
+
+        Return True iff this CashRegister has the same amount of money as other.
+        
+        >>> reg1 = CashRegister(2, 0, 0, 0, 0)
+        >>> reg2 = CashRegister(0, 1, 0, 0, 0)
+        >>> reg1 == reg2
+        True
+        """
+
+        return self.get_total() == other.get_total()
+
     def get_total(self):
         """ (CashRegister) -> int
 
         Return the total amount of cash in the register.
-        
-        >>> register = CashRegister(5, 5, 5, 5, 5)
-        >>> register.get_total()
-        190
         """
 
         return self.loonies + self.toonies * 2 + self.fives * 5 + \
@@ -44,14 +41,6 @@ class CashRegister:
         Add count items of denomination to the register.
         denomination is one of 'loonies', 'toonies',
         'fives', 'tens', and 'twenties'.
-        
-        >>> register = CashRegister(5, 5, 5, 5, 5)
-        >>> register.add(2, 'toonies')
-        >>> register.toonies
-        7
-        >>> register.add(1, 'tens')
-        >>> register.tens
-        6
         """
 
         if denomination == 'loonies':
@@ -72,14 +61,6 @@ class CashRegister:
         Remove count items of denomination from the register.
         denomination is one of 'loonies', 'toonies',
         'fives', 'tens', and 'twenties'.
-        
-        >>> register = CashRegister(5, 5, 5, 5, 5)
-        >>> register.remove(2, 'toonies')
-        >>> register.toonies
-        3
-        >>> register.remove(1, 'tens')
-        >>> register.tens
-        4
         """
 
         if denomination == 'loonies':
@@ -95,12 +76,10 @@ class CashRegister:
 
 
 if __name__ == '__main__':
-    # A cash register with 5 loonies, 5 toonies, 5 fives, 5 tens, and 5 twenties,
-    # for a total of $190.
-    register = CashRegister(5, 5, 5, 5, 5)
-    print(register.get_total())
 
-    register.add(3, 'toonies')
-    register.remove(2, 'twenties')
-
-    print(register.get_total())
+    cr1 = CashRegister(2, 0, 0, 0, 0)
+    cr2 = CashRegister(0, 1, 0, 0, 0)
+    cr3 = CashRegister(1, 1, 0, 0, 0)
+    print(cr1 == cr2)
+    print(cr3 == cr2)
+    
