94e1f05a28f0001411127976e0e24756a0ba00bc
Khalid Hassan <khalidhsoliman@gmail.com>
2018-05-14 04:01:52 +0200

    Add test cases for swap_k()

--- Week2/Peer-graded Assignment Writing test cases/test_swap_k.py
+++ Week2/Peer-graded Assignment Writing test cases/test_swap_k.py
@@ -7,6 +7,78 @@ class TestSwapK(unittest.TestCase):
 
     # Add your test methods for a1.swap_k here.
 
+    def test_swap_k_empty_zero(self):
+        """
+        Test swap_k with empty list and k = 0 (boundry)
+        """
+
+        L = []
+        L_expected = []
+
+        a1.swap_k(L, 0)
+
+        self.assertEqual(L, L_expected)
+
+    def test_swap_k_full_zero(self):
+        """
+        Test swap_k with a list of numbers and k = 0
+        """
+
+        L = [1, 2, 3, 4, 5, 6]
+        L_expected = [1, 2, 3, 4, 5, 6]
+
+        a1.swap_k(L, 0)
+
+        self.assertEqual(L, L_expected)
+
+    def test_swap_k_full_max(self):
+        """
+        Test swap_k with a list of numbers and k = len(L)// 2
+        """
+
+        L = [1, 2, 3, 4, 5, 6]
+        L_expected = [4, 5, 6, 1, 2, 3]
+
+        a1.swap_k(L, 3)
+
+        self.assertEqual(L, L_expected)
+
+    def test_swap_k_inbetween_even(self):
+        """
+        Test swap_k with a list of even number and k = 2
+        """
+
+        L = [1, 2, 3, 4, 5, 6]
+        L_expected = [5, 6, 3, 4, 1, 2]
+
+        a1.swap_k(L, 2)
+
+        self.assertEqual(L, L_expected)
+
+    def test_swap_k_inbetween_odd(self):
+        """
+        Test swap_k with a list of odd numbers and k = 1
+        """
+
+        L = [1, 2, 3]
+        L_expected = [3, 2, 1]
+
+        a1.swap_k(L, 1)
+
+        self.assertEqual(L, L_expected) 
+
+        
+    def test_swap_k_smallest_list(self):
+        """
+        Test swap_k with a list of two number and k = 1
+        """
+
+        L = [1, 2]
+        L_expected = [2, 1]
+
+        a1.swap_k(L, 1)
+
+        self.assertEqual(L, L_expected)
 
 if __name__ == '__main__':
     unittest.main(exit=False)
