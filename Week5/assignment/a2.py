eff96edcd9c43e18282a1dee46f0fa07abc6a331
Khalid Hassan <khalidhsoliman@gmail.com>
2018-05-15 05:09:00 +0200

    Write class Rat's methods.

--- Week5/assignment/a2.py
+++ Week5/assignment/a2.py
@@ -37,7 +37,48 @@ class Rat:
     """ A rat caught in a maze. """
 
     # Write your Rat methods here.
+    def __init__(self, symbol, row, col):
+        """ (Rat, str, int, int) => NoneType
+    
+        initialize a Rat with a symbol to show on the maze
+        and two int to detect its position
+        """
+        
+        self.symbol =symbol
+        self.row = row
+        self.col = col
+        self.num_sprouts_eaten = 0
 
+    def set_location(self, row, col):
+        """ (Rat, int, int) => NoneType
+
+        Set the rat's row and col instance variables to
+        the given row and column.
+        """
+        
+        self.row = row
+        self.col = col
+
+    def eat_sprout(self):
+        """ (Rat) => NoneType
+
+        Add one to the rat's instance variable
+        num_sprouts_eaten
+        """
+
+        self.num_sprouts_eaten+=1
+
+    def __str__(self):
+        """ (Rat) => str
+
+        Return a string representation of the rat,
+        in this format:
+        symbol at (row, col) ate num_sprouts_eaten sprouts.    
+        """
+
+        return '{0} at ({1}, {2}) ate {3} sprouts'.format(
+            self.symbol,self.row,self.col,self.num_sprouts_eaten)
+    
 class Maze:
     """ A 2D maze. """
 
